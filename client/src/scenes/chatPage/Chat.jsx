0f7600154ab4fc6fca83961287c5304b524eb3cd
kunwarmahmad <makunwar1@gmail.com>
2024-04-14 20:05:05 +0500

    Integrated Chat Module

--- client/src/scenes/chatPage/Chat.jsx
+++ client/src/scenes/chatPage/Chat.jsx
@@ -1,72 +1,17 @@
 // ChatPage.jsx
-import React, { useEffect, useRef, useState } from "react";
-import { Box, useMediaQuery } from "@mui/material";
+import React from "react";
+import { Box } from "@mui/material";
 import Navbar from "scenes/navbar";
 import "./Chat.css";
-import { userChats } from "../../api/ChatRequests";
 
-import { useSelector } from "react-redux";
-import { io } from "socket.io-client";
 
 import ChatParent from "./ChatParent";
 
 const ChatPage = () => {
-  const socket = useRef();
-  const user = useSelector((state) => state.user);
-  const [chats, setChats] = useState([]);
-  const [currentChat, setCurrentChat] = useState(null);
-  const [onlineUsers, setOnlineUsers] = useState([]);
-  const [sendMessage, setSendMessage] = useState(null);
-  const [receivedMessage, setReceivedMessage] = useState(null);
-  const [isChatClicked, setIsChatClicked] = useState(false);
-  const isMobileScreens = useMediaQuery("(min-width:768px)");
-
-  useEffect(() => {
-    const getChats = async () => {
-      try {
-        const { data } = await userChats(user._id);
-        setChats(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getChats();
-  }, [user]);
-
-  useEffect(() => {
-    if (sendMessage !== null) {
-      socket.current.emit("send-message", sendMessage);
-    }
-  }, [sendMessage]);
-
-  useEffect(() => {
-    socket.current = io("ws://localhost:8800");
-    socket.current.emit("new-user-add", user._id);
-    socket.current.on("get-users", (users) => {
-      setOnlineUsers(users);
-    });
-  }, [user]);
-
-  useEffect(() => {
-    socket.current.on("recieve-message", (data) => {
-      setReceivedMessage(data);
-    });
-  }, []);
-
   return (
     <Box>
       <Navbar />
-      <ChatParent
-        chats={chats}
-        user={user}
-        currentChat={currentChat}
-        setCurrentChat={setCurrentChat}
-        setIsChatClicked={setIsChatClicked}
-        isMobileScreens={isMobileScreens}
-        onlineUsers={onlineUsers}
-        setSendMessage={setSendMessage}
-        receivedMessage={receivedMessage}
-      />
+      <ChatParent />
     </Box>
   );
 };
--- client/src/scenes/chatPage/ChatParent.jsx
+++ client/src/scenes/chatPage/ChatParent.jsx
@@ -1,10 +1,59 @@
 // ChatParent.jsx
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useMediaQuery } from "@mui/material";
+import "./Chat.css";
+import { userChats } from "../../api/ChatRequests";
+
+import { useSelector } from "react-redux";
+import { io } from "socket.io-client";
 import Conversation from "components/Conversation";
 import ChatBox from "components/ChatBox";
 import WidgetWrapper from "components/WidgetWrapper";
 
-const ChatParent = ({ chats, user, currentChat, setCurrentChat, setIsChatClicked, isMobileScreens, onlineUsers, setSendMessage, receivedMessage }) => {
+
+const ChatParent = () => {
+  const socket = useRef();
+  const user = useSelector((state) => state.user);
+  const [chats, setChats] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [sendMessage, setSendMessage] = useState(null);
+  const [receivedMessage, setReceivedMessage] = useState(null);
+  const [isChatClicked, setIsChatClicked] = useState(false);
+  const isMobileScreens = useMediaQuery("(min-width:768px)");
+
+  useEffect(() => {
+    const getChats = async () => {
+      try {
+        const { data } = await userChats(user._id);
+        setChats(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getChats();
+  }, [user]);
+
+  useEffect(() => {
+    if (sendMessage !== null) {
+      socket.current.emit("send-message", sendMessage);
+    }
+  }, [sendMessage]);
+
+  useEffect(() => {
+    socket.current = io("ws://localhost:8800");
+    socket.current.emit("new-user-add", user._id);
+    socket.current.on("get-users", (users) => {
+      setOnlineUsers(users);
+    });
+  }, [user]);
+
+  useEffect(() => {
+    socket.current.on("recieve-message", (data) => {
+      setReceivedMessage(data);
+    });
+  }, []);
+
   const checkOnlineStatus = (chat) => {
     const chatMember = chat.members.find((member) => member !== user._id);
     const online = onlineUsers.find((user) => user.userId === chatMember);
@@ -14,7 +63,7 @@ const ChatParent = ({ chats, user, currentChat, setCurrentChat, setIsChatClicked
   return (
     <div className="Chat">
       {/* Left Side */}
-      <div className={`Left-side-chat ${currentChat && !isMobileScreens ? 'hide' : ''}`}>
+      <div className={`Left-side-chat ${isChatClicked && !isMobileScreens ? 'hide' : ''}`}>
         <div className="Chat-container">
           <WidgetWrapper>
             <h2>Chats</h2>
@@ -36,7 +85,7 @@ const ChatParent = ({ chats, user, currentChat, setCurrentChat, setIsChatClicked
       </div>
 
       {/* Right Side */}
-      <div className={`Right-side-chat ${!isMobileScreens && !currentChat ? 'hide' : 'show'}`}>
+      <div className={`Right-side-chat ${!isMobileScreens && !isChatClicked ? 'hide' : 'show'}`}>
         <ChatBox
           chat={currentChat}
           currentUser={user._id}
